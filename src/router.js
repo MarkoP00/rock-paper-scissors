28054529c0c31f79ec8c7a94c3e32af011866a98
MarkoP00 <135166893+MarkoP00@users.noreply.github.com>
2024-05-28 04:29:47 +0200

    Update router.js

--- src/router.js
+++ src/router.js
@@ -1,8 +1,8 @@
-import { createRouter, createWebHistory } from "vue-router";
+import { createRouter, createWebHashHistory } from "vue-router";
 import StartingMenu from './pages/StartingMenu.vue';
 import GamePage from "./pages/GamePage.vue";
 const router = createRouter({
-    history: createWebHistory(),
+    history: createWebHashHistory(),
 
     routes: [
       {
